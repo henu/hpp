30adb7d73eec124c254502bcd55a48c8049c5ec4
henu <henu@70351bcb-05e8-4b4c-b5e1-5e11eabe0d38>
2011-04-25 09:26:35 +0000

    Assert to its own .cc file to reduce includes in header file.

--- assert.h
+++ assert.h
@@ -1,11 +1,7 @@
 #ifndef HPP_ASSERT_H
 #define HPP_ASSERT_H
 
-#include "exception.h"
-
 #include <string>
-#include <cstdlib>
-#include <iostream>
 
 #ifndef NDEBUG
 #define HppAssert(check, error) Hpp::doAssertion(__FILE__, __LINE__, __FUNCTION__, (check), #check, error);
@@ -16,25 +12,7 @@
 namespace Hpp
 {
 
-inline void doAssertion(std::string const& file, size_t line, std::string const& func, bool check, std::string const& check_str, std::string const& msg)
-{
-	if (check) {
-		return;
-	}
-	std::cerr << "ERROR: An assertion has failed!" << std::endl;
-	std::cerr << "Condition: " << check_str << std::endl;
-	std::cerr << "File:      " << file << std::endl;
-	std::cerr << "Line:      " << line << std::endl;
-	std::cerr << "Function:  " << func << std::endl;
-	if (!msg.empty()) {
-		std::cerr << "Message:   " << msg << std::endl;
-	}
-	#ifdef HPP_ASSERT_ABORTS
-	abort();
-	#else
-	throw Exception("Assertion has failed!");
-	#endif
-}
+void doAssertion(std::string const& file, size_t line, std::string const& func, bool check, std::string const& check_str, std::string const& msg);
 
 }
 
