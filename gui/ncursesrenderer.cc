466f14e7c2b320c97ccee996a2e66c2fddb6b2ac
henu <henu@70351bcb-05e8-4b4c-b5e1-5e11eabe0d38>
2012-02-08 20:16:23 +0000

    Gui: New incomplete Renderer for NCurses: NCursesRenderer

---
+++ gui/ncursesrenderer.cc
@@ -0,0 +1,258 @@
+#include "ncursesrenderer.h"
+
+#include "scrollbar.h"
+
+#include "../ncios.h"
+
+namespace Hpp
+{
+
+namespace Gui
+{
+
+NCursesRenderer::NCursesRenderer(void) :
+cursor_col(0), cursor_row(0),
+limit(false)
+{
+}
+
+NCursesRenderer::~NCursesRenderer(void)
+{
+}
+
+void NCursesRenderer::setCursor(int32_t col, int32_t row)
+{
+	cursor_col = col;
+	cursor_row = row;
+	positionCursorIfAtRenderarea();
+}
+
+void NCursesRenderer::setColors(NC::Color text, NC::Color bg)
+{
+	ncout.setColors(text, bg);
+}
+
+void NCursesRenderer::printChar(char c)
+{
+	size_t visible_left = getVisibleLeft();
+	if (visible_left > 0) {
+		ncout << c << std::flush;
+	}
+	advanceCursor(1);
+}
+
+void NCursesRenderer::printString(Hpp::UnicodeString const& str)
+{
+	size_t ofs = 0;
+	while (ofs < str.size()) {
+		size_t visible_left = getVisibleLeft();
+		if (visible_left > 0) {
+			size_t print_amount = std::min(visible_left, str.size() - ofs);
+			ncout << str.substr(ofs, print_amount) << std::flush;
+			ofs += print_amount;
+			advanceCursor(print_amount);
+		} else {
+			size_t hidden_left = getHiddenLeft();
+			size_t print_amount = std::min(hidden_left, str.size() - ofs);
+			ofs += print_amount;
+			advanceCursor(print_amount);
+		}
+	}
+}
+
+void NCursesRenderer::positionCursorIfAtRenderarea(void)
+{
+	if (limit &&
+	    (cursor_col < (int32_t)limit_x || cursor_col >= (int32_t)(limit_x + limit_w) ||
+	     cursor_row < (int32_t)limit_y || cursor_row >= (int32_t)(limit_y + limit_h))) {
+		return;
+	}
+	if (cursor_col < 0 || cursor_col >= (int32_t)ncout.getScreenWidth() ||
+	    cursor_row < 0 || cursor_row >= (int32_t)ncout.getScreenHeight()) {
+		return;
+	}
+	ncout.setLoc(cursor_col, cursor_row);
+}
+
+void NCursesRenderer::advanceCursor(size_t amount)
+{
+	cursor_col += amount;
+	positionCursorIfAtRenderarea();
+}
+
+size_t NCursesRenderer::getVisibleLeft(void) const
+{
+	if (!limit) {
+		if (cursor_row < 0) return 0;
+		if (cursor_row > (int32_t)ncout.getScreenHeight()) return 0;
+		if (cursor_col < 0) return 0;
+		int32_t scr_w = ncout.getScreenWidth();
+		if (cursor_col < scr_w) return scr_w - cursor_col;
+		return 0;
+	}
+	if (cursor_row >= (int32_t)(limit_y + limit_h)) return 0;
+	if (cursor_row >= (int32_t)limit_y) {
+		if (cursor_col < (int32_t)(limit_x)) return 0;
+		if (cursor_col < (int32_t)(limit_x + limit_w)) return limit_x + limit_w - cursor_col;
+		return 0;
+	}
+	return 0;
+}
+
+size_t NCursesRenderer::getHiddenLeft(void) const
+{
+	if (!limit) {
+		if (cursor_row < 0) return (size_t)-1;
+		if (cursor_row > (int32_t)ncout.getScreenHeight()) return (size_t)-1;
+		if (cursor_col < 0) return -cursor_col;
+		int32_t scr_w = ncout.getScreenWidth();
+		if (cursor_col < scr_w) return 0;
+		return (size_t)-1;
+	}
+	if (!limit) return 0;
+	if (cursor_row >= (int32_t)(limit_y + limit_h)) return (size_t)-1;
+	if (cursor_row >= (int32_t)limit_y) {
+		if (cursor_col < (int32_t)limit_x) return limit_x - cursor_col;
+		if (cursor_col < (int32_t)(limit_x + limit_w)) return 0;
+		return (size_t)-1;
+	}
+	return (size_t)-1;
+}
+
+uint32_t NCursesRenderer::getWidth(void) const
+{
+	return ncout.getScreenWidth();
+}
+
+uint32_t NCursesRenderer::getHeight(void) const
+{
+	return ncout.getScreenHeight();
+}
+
+void NCursesRenderer::initRendering(void)
+{
+}
+
+void NCursesRenderer::deinitRendering(void)
+{
+	ncout.refresh();
+}
+
+void NCursesRenderer::renderMenubarBackground(int32_t x_origin, int32_t y_origin, uint32_t width, AreaWithMenubar const* areawithmenubar)
+{
+	setCursor(x_origin, y_origin);
+	setColors(NC::BLACK, NC::GRAY);
+	printString(std::string(width, ' '));
+}
+
+void NCursesRenderer::renderMenuLabel(int32_t x_origin, int32_t y_origin, Menu const* menu, UnicodeString const& label, bool mouse_over)
+{
+	setCursor(x_origin, y_origin);
+	setColors(NC::BLACK, NC::GRAY);
+	printChar(' ');
+	printString(label);
+	printChar(' ');
+}
+
+void NCursesRenderer::renderMenuseparator(int32_t x_origin, int32_t y_origin, Menuseparator const* menusep)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderMenuitem(int32_t x_origin, int32_t y_origin, Menuitem const* menuitem, UnicodeString const& label, bool mouse_over)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderWindow(int32_t x_origin, int32_t y_origin, Window const* window, UnicodeString const& title)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderLabel(int32_t x_origin, int32_t y_origin, Label const* label, UnicodeString const& label_str)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderTextinput(int32_t x_origin, int32_t y_origin, Textinput const* textinput)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderTextinputContents(int32_t x_origin, int32_t y_origin, TextinputContents const* textinputcontents, ssize_t cursor)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderButton(int32_t x_origin, int32_t y_origin, Button const* button, UnicodeString const& label, bool pressed)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderFolderview(int32_t x_origin, int32_t y_origin, Folderview const* folderview)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderFolderviewContents(int32_t x_origin, int32_t y_origin, FolderviewContents const* folderviewcontents, FolderChildren const& items)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderScrollbar(int32_t x_origin, int32_t y_origin, Scrollbar const* scrollbar, bool horizontal, bool up_or_left_key_pressed, bool down_or_right_key_pressed, bool slider_pressed)
+{
+	if (horizontal) {
+		setCursor(x_origin, y_origin);
+		setColors(NC::BLACK, NC::GRAY);
+		printChar('<');
+
+		setColors(NC::BLACK, NC::BLACK);
+		printString(std::string(scrollbar->getWidth() - 2, '-'));
+
+		setColors(NC::BLACK, NC::GRAY);
+		printChar('>');
+	} else {
+		setCursor(x_origin, y_origin);
+		setColors(NC::BLACK, NC::GRAY);
+		printChar('^');
+
+		setColors(NC::BLACK, NC::BLACK);
+		for (uint32_t row = 1; row < getHeight() - 1; row ++) {
+			setCursor(x_origin, y_origin + row);
+			printChar('|');
+		}
+
+		setCursor(x_origin, y_origin + getHeight() - 1);
+		setColors(NC::BLACK, NC::GRAY);
+		printChar('v');
+	}
+}
+
+void NCursesRenderer::renderSlider(int32_t x_origin, int32_t y_origin, Slider const* slider, bool horizontal, bool slider_pressed)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::renderTabs(int32_t x_origin, int32_t y_origin, Tabs const* tabs)
+{
+HppAssert(false, "Not implemented yet!");
+}
+
+void NCursesRenderer::setRenderareaLimit(uint32_t x, uint32_t y, uint32_t width, uint32_t height)
+{
+	limit_x = x;
+	limit_y = y;
+	limit_w = width;
+	limit_h = height;
+	limit = true;
+	positionCursorIfAtRenderarea();
+}
+
+void NCursesRenderer::removeRenderareaLimit(void)
+{
+	limit = false;
+}
+
+}
+
+}
---
+++ gui/ncursesrenderer.h
@@ -0,0 +1,119 @@
+#ifndef HPP_GUI_NCURSESRENDERER_H
+#define HPP_GUI_NCURSESRENDERER_H
+
+#include "renderer.h"
+#include "../ncios.h"
+
+namespace Hpp
+{
+
+namespace Gui
+{
+
+class NCursesRenderer : public Renderer
+{
+
+public:
+
+	NCursesRenderer(void);
+	virtual ~NCursesRenderer(void);
+
+	// Helper functions for writing stuff to screen
+	void setCursor(int32_t col, int32_t row);
+	void setColors(NC::Color text, NC::Color bg);
+	void printChar(char c);
+	void printString(Hpp::UnicodeString const& str);
+	inline void printString(std::string const& str) { printString(Hpp::UnicodeString(str)); }
+
+private:
+
+	// Cursor position
+	int32_t cursor_col, cursor_row;
+
+	// Renderarea limit
+	bool limit;
+	uint32_t limit_x, limit_y;
+	uint32_t limit_w, limit_h;
+
+	void positionCursorIfAtRenderarea(void);
+
+	void advanceCursor(size_t amount);
+
+	size_t getVisibleLeft(void) const;
+	size_t getHiddenLeft(void) const;
+
+	// Virtual functions required by superclass Renderer
+	virtual uint32_t getWidth(void) const;
+	virtual uint32_t getHeight(void) const;
+	virtual void initRendering(void);
+	virtual void deinitRendering(void);
+	virtual void renderMenubarBackground(int32_t x_origin, int32_t y_origin, uint32_t width, AreaWithMenubar const* areawithmenubar);
+	virtual void renderMenuLabel(int32_t x_origin, int32_t y_origin, Menu const* menu, UnicodeString const& label, bool mouse_over);
+	virtual void renderMenuseparator(int32_t x_origin, int32_t y_origin, Menuseparator const* menusep);
+	virtual void renderMenuitem(int32_t x_origin, int32_t y_origin, Menuitem const* menuitem, UnicodeString const& label, bool mouse_over);
+	virtual void renderWindow(int32_t x_origin, int32_t y_origin, Window const* window, UnicodeString const& title);
+	virtual void renderLabel(int32_t x_origin, int32_t y_origin, Label const* label, UnicodeString const& label_str);
+	virtual void renderTextinput(int32_t x_origin, int32_t y_origin, Textinput const* textinput);
+	virtual void renderTextinputContents(int32_t x_origin, int32_t y_origin, TextinputContents const* textinputcontents, ssize_t cursor);
+	virtual void renderButton(int32_t x_origin, int32_t y_origin, Button const* button, UnicodeString const& label, bool pressed);
+	virtual void renderFolderview(int32_t x_origin, int32_t y_origin, Folderview const* folderview);
+	virtual void renderFolderviewContents(int32_t x_origin, int32_t y_origin, FolderviewContents const* folderviewcontents, FolderChildren const& items);
+	virtual void renderScrollbar(int32_t x_origin, int32_t y_origin, Scrollbar const* scrollbar, bool horizontal, bool up_or_left_key_pressed, bool down_or_right_key_pressed, bool slider_pressed);
+	virtual void renderSlider(int32_t x_origin, int32_t y_origin, Slider const* slider, bool horizontal, bool slider_pressed);
+	virtual void renderTabs(int32_t x_origin, int32_t y_origin, Tabs const* tabs);
+	inline virtual uint32_t getMenubarHeight(void) const { return 1; }
+	inline virtual uint32_t getMenuLabelWidth(UnicodeString const& label) const { return label.size() + 2; }
+	inline virtual uint32_t getMenuseparatorMinWidth(void) const { return 1; }
+	inline virtual uint32_t getMenuseparatorHeight(void) const { return 1; }
+	inline virtual uint32_t getMenuitemWidth(UnicodeString const& label) const { return label.size(); }
+	inline virtual uint32_t getMenuitemHeight(void) const { return 1; }
+	inline virtual uint32_t getWindowTitlebarHeight(void) const { return 1; }
+	inline virtual uint32_t getWindowEdgeTopHeight(void) const { return 0; }
+	inline virtual uint32_t getWindowEdgeLeftWidth(void) const { return 0; }
+	inline virtual uint32_t getWindowEdgeRightWidth(void) const { return 0; }
+	inline virtual uint32_t getWindowEdgeBottomHeight(void) const { return 0; }
+	inline virtual uint32_t getWindowDragcornerSize(void) const { return 0; }
+	inline virtual uint32_t getLabelWidth(UnicodeString const& label) const { return label.size(); }
+	inline virtual uint32_t getLabelHeight(void) const { return 1; }
+	inline virtual uint32_t getTextinputWidth(size_t cols) const { return cols; }
+	inline virtual uint32_t getTextinputHeight(void) const { return 1; }
+	inline virtual uint32_t getMinimumTextinputContentsWidth(UnicodeString const& value) const { return 1; }
+	inline virtual uint32_t getTextinputContentsHeight(void) const { return 1; }
+	inline virtual void getTextinputContentsCursorProps(uint32_t& cursor_pos_x, uint32_t& cursor_pos_y, uint32_t& cursor_width, uint32_t& cursor_height, UnicodeString const& value, ssize_t cursor) const { cursor_pos_x = cursor; cursor_pos_y = 0; cursor_width = 1; cursor_height = 1; }
+	inline virtual void getTextinputEdgeSizes(uint32_t& edge_top, uint32_t& edge_left, uint32_t& edge_right, uint32_t& edge_bottom) const { edge_top = 0; edge_left = 0; edge_right = 0; edge_bottom = 0; }
+	inline virtual uint32_t getButtonWidth(UnicodeString const& label) const { return label.size(); }
+	inline virtual uint32_t getButtonHeight(void) const { return 1; }
+	inline virtual uint32_t getMinimumFolderviewWidth(void) const { return 0; }
+	inline virtual uint32_t getFolderviewHeight(void) const { return 1; }
+	inline virtual uint32_t getMinimumFolderviewContentsWidth(UnicodeString const& label) const { return 0; }
+	inline virtual uint32_t getFolderviewContentsHeight(size_t items) const { return 1; }
+	inline virtual void getFolderviewEdgeSizes(uint32_t& edge_top, uint32_t& edge_left, uint32_t& edge_right, uint32_t& edge_bottom) const { edge_top = 0; edge_left = 0; edge_right = 0; edge_bottom = 0; }
+	inline virtual uint32_t getScrollbarWidth(void) const { return 1; }
+	inline virtual uint32_t getScrollbarHeight(void) const { return 1; }
+	inline virtual uint32_t getScrollbarButtonLeftWidth(void) const { return 1; }
+	inline virtual uint32_t getScrollbarButtonRightWidth(void) const { return 1; }
+	inline virtual uint32_t getScrollbarButtonUpHeight(void) const { return 1; }
+	inline virtual uint32_t getScrollbarButtonDownHeight(void) const { return 1; }
+	inline virtual uint32_t getScrollboxMinWidth(void) const { return 2; }
+	inline virtual uint32_t getScrollboxMinHeight(void) const { return 2; }
+	inline virtual uint32_t getHorizSliderWidth(void) const { return 1; }
+	inline virtual uint32_t getVertSliderHeight(void) const { return 1; }
+	inline virtual uint32_t getHorizSliderMinWidth(void) const { return 2; }
+	inline virtual uint32_t getVertSliderMinHeight(void) const { return 2; }
+	inline virtual uint32_t getHorizSliderHeight(void) const { return 1; }
+	inline virtual uint32_t getVertSliderWidth(void) const { return 1; }
+	inline virtual uint32_t getTabbarHeight(void) const { return 1; }
+	inline virtual uint32_t getTabsLeftEdgeWidth(void) const { return 0; }
+	inline virtual uint32_t getTabsRightEdgeWidth(void) const { return 0; }
+	inline virtual uint32_t getTabsBottomEdgeHeight(void) const { return 0; }
+	inline virtual uint32_t getTablabelWidth(UnicodeString const& label) const { return label.size() + 2; }
+	virtual void setRenderareaLimit(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
+	virtual void removeRenderareaLimit(void);
+
+};
+
+}
+
+}
+
+#endif
