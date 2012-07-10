e15d7e9cbe479fbb76e1c5694d3a979c3d143dea
henu <henu@70351bcb-05e8-4b4c-b5e1-5e11eabe0d38>
2012-07-10 21:32:30 +0000

    random.h: New method: randomSecureData()

--- random.h
+++ random.h
@@ -6,8 +6,10 @@
 #include "angle.h"
 #include "assert.h"
 #include "real.h"
+#include "bytev.h"
 
 #include <cstdlib>
+#include <fstream>
 
 namespace Hpp
 {
@@ -18,10 +20,14 @@ inline Real random(Real min, Real max);
 // Returns a value in range [min, max]
 inline ssize_t randomInt(ssize_t min, ssize_t max);
 
+// Returns random angle between [-180 - 180) degrees.
 inline Angle randomAngle(void);
 
 inline Vector2 randomVector2(Real max_radius);
 inline Vector3 randomVector3(Real max_radius);
+
+// Returns data from secure random source
+inline ByteV randomSecureData(size_t size);
 
 inline Real random(Real min, Real max)
 {
@@ -60,6 +66,31 @@ inline Vector3 randomVector3(Real max_radius)
 		result.y = random(-max_radius, max_radius);
 		result.z = random(-max_radius, max_radius);
 	} while (result.lengthTo2() > max_radius_to_2);
+	return result;
+}
+
+inline ByteV randomSecureData(size_t size)
+{
+	ByteV result;
+
+	#ifndef WIN32
+
+	std::ifstream rnd("/dev/random");
+	if (!rnd.is_open()) {
+		throw Exception("Unable to open /dev/random!");
+	}
+
+	result.assign(size, 0);
+	rnd.read((char*)&result[0], size);
+
+	rnd.close();
+
+	#else
+
+	HppAssert(false, "Not implemented for windows yet!");
+
+	#endif
+
 	return result;
 }
 
