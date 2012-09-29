ab39588e826e8487c525f35e96e290ce87ce4dad
henu <henu@70351bcb-05e8-4b4c-b5e1-5e11eabe0d38>
2012-09-29 12:10:29 +0000

    Arguments::getHelp() improved.

--- arguments.h
+++ arguments.h
@@ -15,6 +15,15 @@ class Arguments
 
 public:
 
+	// What to output in help
+	static uint8_t const INC_ARG             = 0x01;
+	static uint8_t const INC_ALIASES         = 0x02;
+	static uint8_t const INC_PARAMS          = 0x04;
+	static uint8_t const INC_DESC            = 0x08;
+	static uint8_t const INC_ARG_AND_ALIASES = 0x03;
+	static uint8_t const INC_ALL_BUT_DESC    = 0x07;
+	static uint8_t const INC_ALL             = 0xff;
+
 	inline Arguments(int argc, char const** argv);
 	inline Arguments(int argc, char** argv);
 
@@ -34,8 +43,9 @@ public:
 
 	inline size_t argsLeft(void) const;
 
-	// Forms help string of all allowed arguments.
-	inline std::string getHelp(void) const;
+	// Forms help string of all allowed arguments,
+	// or just from onle argument, if given.
+	inline std::string getHelp(uint8_t inc_what = INC_ALL, std::string const& arg = "") const;
 
 private:
 
@@ -118,32 +128,76 @@ inline size_t Arguments::argsLeft(void) const
 	return args.end() - args_it;
 }
 
-inline std::string Arguments::getHelp(void) const
+inline std::string Arguments::getHelp(uint8_t inc_what, std::string const& arg) const
 {
 	std::string result;
-	bool first_arg = true;
-	for (AllowedArgs::const_iterator aas_it = aas.begin();
-	     aas_it != aas.end();
-	     aas_it ++) {
-	     	if (!first_arg) {
-	     		result += '\n';
-	     	}
-	     	// Print name, aliases and required parameters of argument.
-		std::string const& arg = aas_it->first;
-		AllowedArg const& aa = aas_it->second;
-		result += arg;
-		for (Aliases::const_iterator aliases_it = aliases.begin();
-		     aliases_it != aliases.end();
-		     aliases_it ++) {
-		     	if (aliases_it->second == arg) {
-		     		result += ", " + aliases_it->first;
+	if (arg == "") {
+		bool first_arg = true;
+		for (AllowedArgs::const_iterator aas_it = aas.begin();
+		     aas_it != aas.end();
+		     aas_it ++) {
+		     	if (!first_arg) {
+		     		result += '\n';
 		     	}
-		}
-		result += " " + aa.params + '\n';
-		// Print description of argument
+		     	// Print name, aliases and required parameters of argument.
+			std::string const& arg2 = aas_it->first;
+			AllowedArg const& aa = aas_it->second;
+			result += arg2;
+			for (Aliases::const_iterator aliases_it = aliases.begin();
+			     aliases_it != aliases.end();
+			     aliases_it ++) {
+			     	if (aliases_it->second == arg2) {
+			     		result += ", " + aliases_it->first;
+			     	}
+			}
+			result += " " + aa.params + '\n';
+			// Print description of argument
 // TODO: Make nice format in future!
-		result += "    " + aa.description + '\n';
-		first_arg = false;
+// TODO: Use inc_what options!
+			result += "    " + aa.description + '\n';
+			first_arg = false;
+		}
+	} else {
+		AllowedArgs::const_iterator aas_find = aas.find(arg);
+		if (aas_find == aas.end()) {
+			Aliases::const_iterator aliases_find = aliases.find(arg);
+			if (aliases_find == aliases.end()) {
+				throw Exception("Argument not found!");
+			}
+			aas_find = aas.find(aliases_find->second);
+			if (aas_find == aas.end()) {
+				throw Exception("Alias found, but it points to non-existing argument!");
+			}
+		}
+		AllowedArg const& aa = aas_find->second;
+
+		if (inc_what & INC_ARG) {
+			result += aas_find->first;
+		}
+		if (inc_what & INC_ALIASES) {
+			for (Aliases::const_iterator aliases_it = aliases.begin();
+			     aliases_it != aliases.end();
+			     ++ aliases_it) {
+				if (aliases_it->second == aas_find->first) {
+					if (!result.empty()) {
+						result += " / ";
+					}
+					result += aliases_it->first;
+				}
+			}
+		}
+		if (inc_what & INC_PARAMS) {
+			if (!result.empty()) {
+				result += " ";
+			}
+			result += aa.params;
+		}
+		if (inc_what & INC_DESC) {
+			if (!result.empty()) {
+				result += " ";
+			}
+			result += aa.description;
+		}
 	}
 	return result;
 }
