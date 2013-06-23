2a95db4013101c8935cc707b536b11896ef16949
Henrik Heino <henu@henu.fi>
2013-06-23 16:00:05 +0300

    Mesh now accepts already constructed Bufferobjects too.

--- mesh.h
+++ mesh.h
@@ -20,14 +20,23 @@ public:
 
 	// The size is not given in bytes, but in elemenst
 	inline void setBuffer(std::string const& name, GLenum target, GLenum type, GLenum usage, GLint components, GLvoid const* data, size_t size);
+	inline void setBuffer(std::string const& name, Bufferobject* bo, bool release_automatically = false);
 
 	inline Bufferobject const* getBuffer(std::string const& name) const;
 
 private:
 
-	typedef std::map< std::string, Bufferobject* > Bufs;
+	struct Buffer
+	{
+		inline Buffer(void) { }
+		inline Buffer(Bufferobject* bo, bool release_automatically) : bo(bo), release_automatically(release_automatically) { }
+		inline Buffer(Buffer const& b) : bo(b.bo), release_automatically(b.release_automatically) { }
+		Bufferobject* bo;
+		bool release_automatically;
+	};
+	typedef std::map< std::string, Buffer > Buffers;
 
-	Bufs bufs;
+	Buffers bufs;
 
 };
 
@@ -37,32 +46,50 @@ inline Mesh::Mesh(void)
 
 inline Mesh::~Mesh(void)
 {
-	for (Bufs::iterator bufs_it = bufs.begin();
+	for (Buffers::iterator bufs_it = bufs.begin();
 	     bufs_it != bufs.end();
 	     ++ bufs_it) {
-		delete bufs_it->second;
+		if (bufs_it->second.release_automatically) {
+			delete bufs_it->second.bo;
+		}
 	}
 }
 
 inline void Mesh::setBuffer(std::string const& name, GLenum target, GLenum type, GLenum usage, GLint components, GLvoid const* data, size_t size)
 {
 	// Remove possible old buffer
-	Bufs::iterator bufs_find = bufs.find(name);
+	Buffers::iterator bufs_find = bufs.find(name);
 	if (bufs_find != bufs.end()) {
-		delete bufs_find->second;
+		if (bufs_find->second.release_automatically) {
+			delete bufs_find->second.bo;
+		}
 	}
 
 	// Create and store new buffer
-	bufs[name] = new Bufferobject(target, type, usage, components, data, size);
+	bufs[name] = Buffer(new Bufferobject(target, type, usage, components, data, size), true);
+}
+
+inline void Mesh::setBuffer(std::string const& name, Bufferobject* bo, bool release_automatically)
+{
+	// Remove possible old buffer
+	Buffers::iterator bufs_find = bufs.find(name);
+	if (bufs_find != bufs.end()) {
+		if (bufs_find->second.release_automatically) {
+			delete bufs_find->second.bo;
+		}
+	}
+
+	// Create and store new buffer
+	bufs[name] = Buffer(bo, release_automatically);
 }
 
 inline Bufferobject const* Mesh::getBuffer(std::string const& name) const
 {
-	Bufs::const_iterator bufs_find = bufs.find(name);
+	Buffers::const_iterator bufs_find = bufs.find(name);
 	if (bufs_find == bufs.end()) {
 		throw Exception("Unable to get buffer \"" + name + "\" from mesh because it does not exist!");
 	}
-	return bufs_find->second;
+	return bufs_find->second.bo;
 }
 
 }
