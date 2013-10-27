a2b8893752596a04882fd520e4e5da33b41ef2ce
Henrik Heino <henu@henu.fi>
2013-10-27 18:19:06 +0200

    Http::Request now supports redirects.

--- http/request.cc
+++ http/request.cc
@@ -8,14 +8,14 @@ namespace Hpp
 namespace Http
 {
 
-Request::Request(std::string const& url) :
+Request::Request(std::string const& url, Flags flags) :
 curl(curl_easy_init())
 {
 	curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, curl_error);
 	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, responseReader);
 	curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
 
-	open(url);
+	open(url, flags);
 }
 
 Request::~Request(void)
@@ -23,12 +23,21 @@ Request::~Request(void)
 	curl_easy_cleanup(curl);
 }
 
-void Request::open(std::string const& url)
+void Request::open(std::string const& url, Flags flags)
 {
+	// Reset response
 	response_data.clear();
 	response_http_code = 0;
 
+	// Refresh options
+	extractFlags(flags);
+
+	// Url
 	curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+	// Redirects
+	if (disable_redirects) curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 0L);
+	else curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
 
 	CURLcode perform_result = curl_easy_perform(curl);
 	if (perform_result != 0) {
@@ -36,6 +45,11 @@ void Request::open(std::string const& url)
 	}
 
 	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_http_code);
+}
+
+void Request::extractFlags(Request::Flags flags)
+{
+	disable_redirects = flags & DISABLE_REDIRECTS;
 }
 
 size_t Request::responseReader(char* ptr, size_t size, size_t nmemb, void* userdata)
--- http/request.h
+++ http/request.h
@@ -19,22 +19,32 @@ class Request : public NonCopyable
 
 public:
 
-	Request(std::string const& url);
+	typedef uint8_t Flags;
+
+	static Flags const DISABLE_REDIRECTS = 0x01;
+
+	Request(std::string const& url, Flags flags = 0);
 	~Request(void);
 
-	void open(std::string const& url);
+	void open(std::string const& url, Flags flags = 0);
 
 	inline std::string getResponseText(void) const { return std::string((char*)&response_data[0], response_data.size()); }
 	inline uint16_t getHttpCode(void) const { return response_http_code; }
 
 private:
 
+	// Curl stuff
 	CURL* curl;
-
 	char curl_error[CURL_ERROR_SIZE];
 
+	// Options
+	bool disable_redirects;
+
+	// Response
 	ByteV response_data;
 	long response_http_code;
+
+	void extractFlags(Flags flags);
 
 	static size_t responseReader(char* ptr, size_t size, size_t nmemb, void* userdata);
 
